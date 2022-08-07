dede38f136f323ab5f10a1774bce4c8615f97ba6
Vishnuraj <vraj54208@gmail.com>
2022-08-07 17:54:56 +0530

    modified

--- sample.js
+++ sample.js
@@ -5,7 +5,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navigation").style.top = "0";
   } else {
-    document.getElementById("navigation").style.top = "-250px";
+    document.getElementById("navigation").style.top = "-20px";
   }
   prevScrollpos = currentScrollPos;
 }
